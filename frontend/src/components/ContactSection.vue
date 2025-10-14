e42d0978a8565af0c9fab19e1ecd4cf9ea6ad150
Sofia Lennbom <sofia.lennbom@destinationgotland.se>
2025-10-14 09:30:22 +0200

    Updated icons for contact.

--- frontend/src/components/ContactSection.vue
+++ frontend/src/components/ContactSection.vue
@@ -1,46 +1,45 @@
-<script>
-// import { CodeBracketIcon } from "@heroicons/vue/24/outline" // If you need icons
+<script setup>
+import { Github, Linkedin, FileText, FileDown } from 'lucide-vue-next';
 
-export default {
-  name: 'ContactSection',
-}
+// export default {
+//   name: 'ContactSection',
+// }
 </script>
 
 <template>
   <div class="flex flex-col items-start justify-center py-4 md:py-8 px-2 md:px-4 space-y-4 mb-4 md:max-w-400 md:mx-auto ">
     <h2 class="font-family-rubik text-2xl md:text-4xl font-bold">Let´s Talk</h2>     
-    <ul class="space-y-2">
-      <li>
-        <a href= "+46705623461" class=" hover:text-gray-900">+46(0)705 62 34 61</a>
-      </li>
-      <li>
-        <a href="sofialennbom@gmail.com" class=" hover:text-main-accent">sofialennbom@gmail.com</a> 
-      </li>
-      </ul>
-      <ul class="flex justify-center items-center space-x-4">
-      <li>
-        <a href="https://www.linkedin.com/in/sofia-lennbom-91252195/" target="_blank" rel="noopener noreferrer">
-          <img 
+    <div class="space-y-2">
+      <a href= "+46705623461" class=" hover:text-gray-900">+46(0)705 62 34 61</a> 
+      <a href="sofialennbom@gmail.com" class=" hover:text-main-accent">sofialennbom@gmail.com</a> 
+    </div>
+    <div class="flex justify-center items-center space-x-4">
+      <a 
+        href="https://www.linkedin.com/in/sofia-lennbom-91252195/" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        class="bg-main-accent rounded-full shadow-lg p-2 hover:bg-main-text transition-colors">
+           <!--  <img 
           src="/assets/LinkedIn-Black.png" 
           alt="LinkedIn icon" 
           class="w-8 h-8  hover:text-main-accent hover:opacity-80 transition-all" 
-        /></a>
-        <!-- Fix icons to hover accent color -->
-      </li>
-      <li>        
-        <a href="https://github.com/solen80a" target="_blank" rel="noopener noreferrer">
-        <img 
+        /> -->
+        <Linkedin class="w-6 h-6 text-main-text hover:text-main-light hover:bg-main-text transition-colors" />
+      </a>
+      <a href="https://github.com/solen80a" target="_blank" rel="noopener noreferrer"
+         class="bg-main-accent rounded-full shadow-lg p-2 hover:bg-main-text transition-colors">
+        <!-- <img 
           src="/assets/github-mark.png" 
           alt="GitHub icon" 
           class="w-8 h-8  hover:text-main-accent hover:opacity-80 transition-all" 
-        />
-        </a>
-      </li>
-      <li>
-        <a href="CV.pdf" class="hover:text-main-accent">CV</a>
-      </li>
-    </ul>  
+        /> -->
+          <Github class="w-6 h-6 text-main-text hover:text-main-light hover:bg-main-text transition-colors" />
+      </a>
+      <a 
+        href="CV.pdf"
+        class="bg-main-accent rounded-full shadow-lg p-2 hover:bg-main-text transition-colors">
+        <FileText class="w-6 h-6 text-main-text hover:text-main-light hover:bg-main-text transition-colors"/>
+      </a>
+    </div>  
   </div>
-     
-      
 </template>
