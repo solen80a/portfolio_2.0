ec8554a8c816359f0a068f97016572e5da3f6f18
Sofia Lennbom <sofia.lennbom@destinationgotland.se>
2025-10-15 15:24:13 +0200

    Update projectmodal styling.

--- frontend/src/components/ProjectModal.vue
+++ frontend/src/components/ProjectModal.vue
@@ -26,12 +26,12 @@ const open = ref(false)
         </TransitionChild>
 
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="flex items-center justify-center p-4 text-center">
             <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to=" translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from=" translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-              <DialogPanel class="relative transform overflow-hidden rounded-lg bg-main text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div class="bg-main px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div class="sm:flex sm:items-start">               
-                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <DialogPanel class="relative transform overflow-hidden rounded-lg bg-main text-left shadow-xl transition-all my-8 w-full max-w-lg">
+                <div class="bg-main px-4 pt-5 p-6 pb-4">
+                  <div class="flex items-start">               
+                    <div class="mt-0 ml-4 text-left">
                       <DialogTitle as="h3" class="text-xl font-semibold">{{ props.project.name }}</DialogTitle>
                       <div class="mt-2">
                         <p class="mb-4">{{ props.project.content }}</p>
@@ -51,7 +51,7 @@ const open = ref(false)
                           <p class="mb-2 text-lg font-semibold">Technologies:</p>
                           <div class="flex flex-wrap gap-2">
                             <!-- <span v-for="tag in props.project.tags" :key="tag" class="px-2 py-1 bg-main-accent rounded-full"> -->
-                            <p v-for="tag in props.project.tags" :key="tag" class="px-2 py-1">
+                            <p v-for="tag in props.project.tags" :key="tag" class="px-2 py-1 border-l-4 border-main-accent">
                               {{ tag }}
                             </p>
                           </div>
@@ -60,8 +60,8 @@ const open = ref(false)
                     </div>
                   </div>
                 </div>
-                <div class="bg-main px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">                
-                  <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-main-accent px-3 py-2 font-semibold shadow-lg transition-colors hover:bg-main-text hover:text-main-light sm:mt-0 sm:w-auto" @click="open = false" ref="cancelButtonRef">Close</button>
+                <div class="bg-main px-4 py-3 flex flex-row-reverse sm:px-6">                
+                  <button type="button" class="mt-3 inline-flex justify-center rounded-md bg-main-accent px-3 py-2 font-semibold shadow-lg transition-colors hover:bg-main-text hover:text-main-light sm:mt-0 w-auto" @click="open = false" ref="cancelButtonRef">Close</button>
                 </div>
               </DialogPanel>
             </TransitionChild>
